fbbc2924538b5bc14a078db8339b624c27bc3474
Angel <abasurco0@gmail.com>
2025-02-21 13:25:34 +0100

    primera aplicacion avanzada de calculadora de IMC

--- app/src/main/java/com/angel/calculadora_imc/MainActivity.kt
+++ app/src/main/java/com/angel/calculadora_imc/MainActivity.kt
@@ -1,20 +1,52 @@
 package com.angel.calculadora_imc
 
+import android.icu.util.IslamicCalendar.CalculationType
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var heightEditText: EditText
+    lateinit var weightEditText: EditText
+    lateinit var calButton: Button
+    lateinit var resultTextView:TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        heightEditText = findViewById(R.id.heightEditText)
+        weightEditText = findViewById(R.id.weightEditText)
+        calButton = findViewById(R.id.calButton)
+        resultTextView = findViewById(R.id.resultTextView)
+
+            calButton.setOnClickListener {
+                val height = heightEditText.text.toString().toFloat()
+                val weight = weightEditText.text.toString().toFloat()
+
+                val result = weight / (height /100).pow(2)
+
+            println("Altura:" + heightEditText.toString())
+                println("Peso" + weightEditText.toString())
+
+                resultTextView.text = "$result"
+
+        }
     }
+
 }
