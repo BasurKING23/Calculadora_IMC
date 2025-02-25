d5260e7e104e1d73d1519d973a75ca104ff665ee
Angel <abasurco0@gmail.com>
2025-02-25 13:27:55 +0100

    final de app

--- app/src/main/java/com/angel/calculadora_imc/MainActivity.kt
+++ app/src/main/java/com/angel/calculadora_imc/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.angel.calculadora_imc
 
+import android.content.ClipDescription
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -8,21 +9,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.addTextChangedListener
 import com.google.android.material.slider.Slider
+import java.util.Locale
 import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
-
+    // importar //
     lateinit var heightTextView: TextView
     lateinit var heightSlider: Slider
-
-    lateinit var weightTextView: TextView
+    lateinit var weightEditText: EditText
     lateinit var weightAddButton: Button
     lateinit var weightMinusButton: Button
-
     lateinit var calButton: Button
     lateinit var resultTextView: TextView
+    lateinit var descriptionTextView: TextView
 
+    // importar //
+    var height = 170.0f
     var weight = 75.0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,39 +39,88 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        // llamadas a botones//
 
         heightTextView = findViewById(R.id.heightTextView)
         heightSlider = findViewById(R.id.heightSlider)
-
-        weightTextView = findViewById(R.id.weightTextView)
+        weightEditText = findViewById(R.id.weightEditText)
         weightAddButton = findViewById(R.id.weightAddButton)
         weightMinusButton = findViewById(R.id.weightMinusButton)
-
         calButton = findViewById(R.id.calButton)
         resultTextView = findViewById(R.id.resultTextView)
+        descriptionTextView = findViewById(R.id.description)
+        // fin de llamada a botones//
 
         heightSlider.addOnChangeListener { slider, value, fromUser ->
-            heightTextView.text = "${value.toInt()}"
+            height = value
+            heightTextView.text = "${height.toInt()}"
 
         }
         weightAddButton.setOnClickListener {
             weight++
-            weightTextView.text = "$weight"
+            weightEditText.setText("$weight")
         }
         weightMinusButton.setOnClickListener {
             weight--
-            weightTextView.text = "$weight"
+            weightEditText.setText("$weight")
+        }
+
+        weightEditText.addTextChangedListener {
+            val text = weightEditText.text.toString()
+            if (text.isNotEmpty()) {
+                if (text.toFloat() > 300) {
+                    weightEditText.setText("300")
+                }
+                if (text.toFloat() < 1) {
+                    weightEditText.setText("1")
+                }
+            }
+
         }
 
 
         calButton.setOnClickListener {
-            val height = 0f //heightEditText.text.toString().toFloat()
-            val weight = 0f //weightEditText.text.toString().toFloat()
 
-            val result = weight / (height / 100).pow(2)
+        }
+            calButton.setOnClickListener {
+                val result = weight / (height / 100).pow(2)
 
-            resultTextView.text = "$result"
+                resultTextView.text = String.format(Locale.getDefault(), "%.2f", result)
 
+                var color = 0
+                var text = 0
+
+
+                when (result){
+                    in 0f..<18.5f ->{
+                        color = R.color.bajo
+                        text = R.string.bajo
+                    }
+                    in 18.5f..<25f ->{
+                        color = R.color.normal
+                        text = R.string.normal
+                    }
+                    in 25f ..<30f ->{
+                        color = R.color.sobrepeso
+                        text = R.string.sobrepeso
+                    }
+                    in 30f ..<35f ->{
+                        color = R.color.obesidad1
+                        text = R.string.obesidad1
+                    }
+                    in 35f ..<40f ->{
+                        color = R.color.obesidad2
+                        text = R.string.obesidad2
+                    }
+                    in 35f ..<40f ->{
+                        color = R.color.obesidad3
+                        text = R.string.obesidad3
+                }
+
+                }
+                resultTextView.setTextColor(getColor(color))
+                descriptionTextView.setTextColor(getColor(color))
+                descriptionTextView.text = getString(text)
         }
     }
 
